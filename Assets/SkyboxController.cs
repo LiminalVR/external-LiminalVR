c3d9d2e1a8c1ed2085e3301d352b6dc2372001d2
Lewis <1010607@student.sae.edu.au>
2019-07-18 17:05:50 +1000

    July 18th Update

---
+++ Assets/SkyboxController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// The skybox controller should run in edit mode to improve the animation workflow
+/// as we want the timeline to be able to make changes.
+/// </summary>
+[ExecuteInEditMode]
+public class SkyboxController : MonoBehaviour
+{
+    public Material SkyboxMaterial;
+
+    [Range(0,5)]
+    public float SunDisk;
+    public float SunSize;
+    public float SunSizeConvergence;
+    public float AtmosphereThickness;
+    public Color SkyTint = Color.white;
+    public Color Ground = Color.white;
+    public float Exposure;
+
+
+
+    private void Update()
+    {
+        SkyboxMaterial.SetFloat("_AtmosphereThickness", AtmosphereThickness);
+        SkyboxMaterial.SetFloat("_SunDisk", SunDisk);
+        SkyboxMaterial.SetFloat("_SunSize", SunSize);
+        SkyboxMaterial.SetFloat("_SunSizeConvergence", SunSizeConvergence);
+        SkyboxMaterial.SetFloat("_Exposure", Exposure);
+    }
+}
