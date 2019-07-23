71cbe9daf1a9c75ea7b77146e44cd9f350e0a701
Lewis <1010607@student.sae.edu.au>
2019-07-23 14:59:41 +1000

    July 23rd Update

---
+++ Assets/ScreenFade.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Liminal.Core.Fader;
+
+public class ScreenFade : MonoBehaviour
+{
+    public void OnEnable()
+    {
+        ScreenFader.Instance.FadeToBlack(2f);
+    }
+
+    public void OnDisable()
+    {
+        ScreenFader.Instance.FadeToClear(2f);
+    }
+}
