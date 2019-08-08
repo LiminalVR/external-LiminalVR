0eb14e33c456f399680d76050f8bd32630e5c466
Lewis <1010607@student.sae.edu.au>
2019-08-08 16:14:22 +1000

    August 8th Update

---
+++ Assets/App.cs
@@ -0,0 +1,32 @@
+﻿using Liminal.Core.Fader;
+using Liminal.SDK.Core;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class App : MonoBehaviour
+{
+    public PlayableDirector Director;
+    public float EndingTime = 196;
+
+    private IEnumerator Start()
+    {
+        yield return UpdateLoop();
+    }
+
+    private IEnumerator UpdateLoop()
+    {
+        yield return new WaitUntil(() => Director.time >= EndingTime);
+
+        ScreenFader.Instance.FadeToBlack(3);
+        yield return ScreenFader.Instance.WaitUntilFadeComplete();
+
+        End();
+    }
+
+    public void End()
+    {
+        ExperienceApp.End();
+    }
+}
